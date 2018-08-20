987b6f0e6fa337c16b1ecd6693e0b5caff467d6c
Maxim <maxim@iMac-Maxim.local>
2018-08-20 11:08:50 +0300

    First Commit
    1. Создание приложения.
    2. Добавление формы для входа.
    3. Адаптация форма для телефонов в альбомной ориентации, планшетов.

--- VK_App/ViewController.swift
+++ VK_App/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var logButton: UIButton!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //coming soon
+    @IBAction func logIning(_ sender: UIButton) {
+        printInfoAboutUser()
+    }
+    
+    func printInfoAboutUser() {
+        print("Логин: \(loginTextField.text ?? "Поле ввода логина пустое")" )
+        print("Пароль: \(passwordTextField.text ?? "Поле ввода пароля пустое")" )
+    }
 
 }
 
